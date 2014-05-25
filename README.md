00149b8066c2f0905954bcff5819027fd4c0552f
Agnes Crepet <agnes.crepet@gmail.com>
2014-05-25 21:49:18 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+Hello Rest
+=================
+
+Hello World application using Spring 4 and Gradle.
+
+Load project into eclipse using `./gradlew eclipse`
+
+Load project into intellij using `./gradlew idea`
+
+Run on tomcat using `./gradlew tomcatRun`
---
+++ src/main/java/com/ninja_squad/hellorest/config/ServletInitializer.java
@@ -0,0 +1,43 @@
+package com.ninja_squad.hellorest.config;
+
+import javax.servlet.Filter;
+import javax.servlet.ServletRegistration.Dynamic;
+
+import org.springframework.web.filter.CharacterEncodingFilter;
+import org.springframework.web.filter.HiddenHttpMethodFilter;
+import org.springframework.web.filter.HttpPutFormContentFilter;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class ServletInitializer extends
+    AbstractAnnotationConfigDispatcherServletInitializer {
+
+  @Override
+  protected Class<?>[] getRootConfigClasses() {
+    return new Class[] { AppConfig.class };
+  }
+
+  @Override
+  protected Class<?>[] getServletConfigClasses() {
+    return new Class[] { WebConfig.class };
+  }
+
+  @Override
+  protected String[] getServletMappings() {
+    return new String[] { "/" };
+  }
+
+  @Override
+  protected void customizeRegistration(Dynamic registration) {
+    registration.setInitParameter("dispatchOptionsRequest", "true");
+  }
+
+  @Override
+  protected Filter[] getServletFilters() {
+    CharacterEncodingFilter charFilter = new CharacterEncodingFilter();
+    charFilter.setEncoding("UTF-8");
+    charFilter.setForceEncoding(true);
+    return new Filter[] { new HiddenHttpMethodFilter(), charFilter,
+        new HttpPutFormContentFilter() };
+  }
+
+}
